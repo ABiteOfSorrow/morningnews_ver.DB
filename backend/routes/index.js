daa5bcaa8b2dd03c49775a7fccc6f882b33efa7d
ABiteOfSorrow <laceican@naver.com>
2022-10-14 04:32:37 +0200

    fix erorr

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -30,7 +30,7 @@ router.post('/add-article', async function(req, res, next) {
         error.push("Contenus vides");
     } 
     
-    if (foundArticle) {
+    if (foundArticle === true) {
         error.push("Cet article est déjà existe");
         alreadyExist = true;
     } 
